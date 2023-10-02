0fdc7e0051473ff6304a3740c2f74a807f365732
Maggie Bao <maggie.bao202@gmail.com>
2023-10-02 01:11:11 -0400

    letter display with ppi done!

--- Distance Measure Test/Test.swift
+++ Distance Measure Test/Test.swift
@@ -2,57 +2,157 @@
 //  Test.swift
 //  Distance Measure Test
 //
-//  Created by Anderson Men on 8/7/23. Edited by Maggie Bao 8/21/23, 8/27/23, 9/10/23.
+//  Created by Anderson Men on 8/7/23. Edited by Maggie Bao 8/21/23, 8/27/23, 9/10/23. 9/30/23
 //
 
 import UIKit
-
-func getCurrentDevicePPI() -> CGFloat {
-    var systemInfo = utsname()
-    uname(&systemInfo)
-    let modelCode = withUnsafePointer(to: &systemInfo.machine) {
-        $0.withMemoryRebound(to: CChar.self, capacity: 1) {
-            ptr in String.init(validatingUTF8: ptr)
+import DevicePpi
+//EXTENSION UIDEVICE IS FOR GETTING MODEL TYPE FROM https://stackoverflow.com/questions/26028918/how-to-determine-the-current-iphone-device-model DONT NEED TO GET MODEL ANYMORE SINCE ALL OF THIS IS BUILT INTO PACKAGE, WE CAN DELETE
+extension UIDevice {
+    static let modelName: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
         }
-    }
 
-    let identifier: String = modelCode ?? "unknown"
+        func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
+            #if os(iOS)
+            switch identifier {
+            case "iPod5,1":                                       return "iPod touch (5th generation)"
+            case "iPod7,1":                                       return "iPod touch (6th generation)"
+            case "iPod9,1":                                       return "iPod touch (7th generation)"
+            case "iPhone3,1", "iPhone3,2", "iPhone3,3":           return "iPhone 4"
+            case "iPhone4,1":                                     return "iPhone 4s"
+            case "iPhone5,1", "iPhone5,2":                        return "iPhone 5"
+            case "iPhone5,3", "iPhone5,4":                        return "iPhone 5c"
+            case "iPhone6,1", "iPhone6,2":                        return "iPhone 5s"
+            case "iPhone7,2":                                     return "iPhone 6"
+            case "iPhone7,1":                                     return "iPhone 6 Plus"
+            case "iPhone8,1":                                     return "iPhone 6s"
+            case "iPhone8,2":                                     return "iPhone 6s Plus"
+            case "iPhone9,1", "iPhone9,3":                        return "iPhone 7"
+            case "iPhone9,2", "iPhone9,4":                        return "iPhone 7 Plus"
+            case "iPhone10,1", "iPhone10,4":                      return "iPhone 8"
+            case "iPhone10,2", "iPhone10,5":                      return "iPhone 8 Plus"
+            case "iPhone10,3", "iPhone10,6":                      return "iPhone X"
+            case "iPhone11,2":                                    return "iPhone XS"
+            case "iPhone11,4", "iPhone11,6":                      return "iPhone XS Max"
+            case "iPhone11,8":                                    return "iPhone XR"
+            case "iPhone12,1":                                    return "iPhone 11"
+            case "iPhone12,3":                                    return "iPhone 11 Pro"
+            case "iPhone12,5":                                    return "iPhone 11 Pro Max"
+            case "iPhone13,1":                                    return "iPhone 12 mini"
+            case "iPhone13,2":                                    return "iPhone 12"
+            case "iPhone13,3":                                    return "iPhone 12 Pro"
+            case "iPhone13,4":                                    return "iPhone 12 Pro Max"
+            case "iPhone14,4":                                    return "iPhone 13 mini"
+            case "iPhone14,5":                                    return "iPhone 13"
+            case "iPhone14,2":                                    return "iPhone 13 Pro"
+            case "iPhone14,3":                                    return "iPhone 13 Pro Max"
+            case "iPhone14,7":                                    return "iPhone 14"
+            case "iPhone14,8":                                    return "iPhone 14 Plus"
+            case "iPhone15,2":                                    return "iPhone 14 Pro"
+            case "iPhone15,3":                                    return "iPhone 14 Pro Max"
+            case "iPhone15,4":                                    return "iPhone 15"
+            case "iPhone15,5":                                    return "iPhone 15 Plus"
+            case "iPhone16,1":                                    return "iPhone 15 Pro"
+            case "iPhone16,2":                                    return "iPhone 15 Pro Max"
+            case "iPhone8,4":                                     return "iPhone SE"
+            case "iPhone12,8":                                    return "iPhone SE (2nd generation)"
+            case "iPhone14,6":                                    return "iPhone SE (3rd generation)"
+            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":      return "iPad 2"
+            case "iPad3,1", "iPad3,2", "iPad3,3":                 return "iPad (3rd generation)"
+            case "iPad3,4", "iPad3,5", "iPad3,6":                 return "iPad (4th generation)"
+            case "iPad6,11", "iPad6,12":                          return "iPad (5th generation)"
+            case "iPad7,5", "iPad7,6":                            return "iPad (6th generation)"
+            case "iPad7,11", "iPad7,12":                          return "iPad (7th generation)"
+            case "iPad11,6", "iPad11,7":                          return "iPad (8th generation)"
+            case "iPad12,1", "iPad12,2":                          return "iPad (9th generation)"
+            case "iPad13,18", "iPad13,19":                        return "iPad (10th generation)"
+            case "iPad4,1", "iPad4,2", "iPad4,3":                 return "iPad Air"
+            case "iPad5,3", "iPad5,4":                            return "iPad Air 2"
+            case "iPad11,3", "iPad11,4":                          return "iPad Air (3rd generation)"
+            case "iPad13,1", "iPad13,2":                          return "iPad Air (4th generation)"
+            case "iPad13,16", "iPad13,17":                        return "iPad Air (5th generation)"
+            case "iPad2,5", "iPad2,6", "iPad2,7":                 return "iPad mini"
+            case "iPad4,4", "iPad4,5", "iPad4,6":                 return "iPad mini 2"
+            case "iPad4,7", "iPad4,8", "iPad4,9":                 return "iPad mini 3"
+            case "iPad5,1", "iPad5,2":                            return "iPad mini 4"
+            case "iPad11,1", "iPad11,2":                          return "iPad mini (5th generation)"
+            case "iPad14,1", "iPad14,2":                          return "iPad mini (6th generation)"
+            case "iPad6,3", "iPad6,4":                            return "iPad Pro (9.7-inch)"
+            case "iPad7,3", "iPad7,4":                            return "iPad Pro (10.5-inch)"
+            case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":      return "iPad Pro (11-inch) (1st generation)"
+            case "iPad8,9", "iPad8,10":                           return "iPad Pro (11-inch) (2nd generation)"
+            case "iPad13,4", "iPad13,5", "iPad13,6", "iPad13,7":  return "iPad Pro (11-inch) (3rd generation)"
+            case "iPad14,3", "iPad14,4":                          return "iPad Pro (11-inch) (4th generation)"
+            case "iPad6,7", "iPad6,8":                            return "iPad Pro (12.9-inch) (1st generation)"
+            case "iPad7,1", "iPad7,2":                            return "iPad Pro (12.9-inch) (2nd generation)"
+            case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":      return "iPad Pro (12.9-inch) (3rd generation)"
+            case "iPad8,11", "iPad8,12":                          return "iPad Pro (12.9-inch) (4th generation)"
+            case "iPad13,8", "iPad13,9", "iPad13,10", "iPad13,11":return "iPad Pro (12.9-inch) (5th generation)"
+            case "iPad14,5", "iPad14,6":                          return "iPad Pro (12.9-inch) (6th generation)"
+            case "AppleTV5,3":                                    return "Apple TV"
+            case "AppleTV6,2":                                    return "Apple TV 4K"
+            case "AudioAccessory1,1":                             return "HomePod"
+            case "AudioAccessory5,1":                             return "HomePod mini"
+            case "i386", "x86_64", "arm64":                       return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
+            default:                                              return identifier
+            }
+            #elseif os(tvOS)
+            switch identifier {
+            case "AppleTV5,3": return "Apple TV 4"
+            case "AppleTV6,2": return "Apple TV 4K"
+            case "i386", "x86_64": return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "tvOS"))"
+            default: return identifier
+            }
+            #endif
+        }
 
-    switch identifier {
-    // Add all known device models and their PPIs here
-    case "iPhone10,1", "iPhone10,4":    // iPhone 8
-        return 326
-    case "iPhone10,2", "iPhone10,5":    // iPhone 8 Plus
-        return 401
-    case "iPhone10,3", "iPhone10,6":    // iPhone X
-        return 458
-    // ... add other device models ...
-    default:
-        // Fallback to a default PPI or perhaps return nil and handle it
-        return 326  // Default PPI for many iPhones like iPhone 6/7/8
-    }
+        return mapToDevice(identifier: identifier)
+    }()
+
 }
+let modelName = UIDevice.modelName //WE DONT NEED THIS
+
+//BELOW IS GETTING PPI FROM GITHUB PPI PACKAGE https://github.com/Clafou/DevicePpi
+let ppi: Double = {
+    switch Ppi.get() {
+    case .success(let ppi):
+        return ppi
+    case .unknown(let bestGuessPpi, let error):
+        //As Apple releases new iOS devices this library needs to be continously updated to add new devices to its built-in list. It cannot guarantee an accurate PPI value for new devices that it does not yet know, but in such cases it provides a "best guess" PPI based on heuristics (UIUserInterfaceIdiom and UIScreen values) and an error code which you can treat as a non-fatal error.
+        // A bestGuessPpi value is provided but may be incorrect
+        // Treat as a non-fatal error -- e.g. log to your backend and/or display a message
+        return bestGuessPpi
+    }
+}()
 
 class Test: UIViewController {
-    var scaleFactor: CGFloat = 50;
+    var scaleFactor: CGFloat = 2;
     var letterText: String = "e";
     var speechRecognizer = SpeechRecognizer();
     @IBOutlet weak var oneLetter: UILabel!
     @IBOutlet weak var tempVoiceText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //oneLetter.font = oneLetter.font.withSize(scaleFactor);
-        let ppi = getCurrentDevicePPI()
+        //print(modelName)
         let pointsFor1Inch = 1 * ppi
-
-        // Assuming you have a UILabel to display the letter
-        let oneLetter = UILabel()
+        print(pointsFor1Inch)
         oneLetter.text = letterText;
-        oneLetter.textAlignment = .center
         oneLetter.frame.size = CGSize(width: pointsFor1Inch, height: pointsFor1Inch)
-        // Set the font size or adjust the label size here if needed
-
-        view.addSubview(oneLetter)
+        oneLetter.font = oneLetter.font.withSize(oneLetter.frame.height)
+        //center letter
+        oneLetter.translatesAutoresizingMaskIntoConstraints = false
+        oneLetter.lineBreakMode = .byWordWrapping
+        oneLetter.textAlignment = .center
+        self.view.addSubview(oneLetter)
+        oneLetter.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        oneLetter.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        oneLetter.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     @IBAction func startIsPressed(_ sender: Any) {
