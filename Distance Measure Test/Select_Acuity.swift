f11e45b3f695b75ed3c83e5d0c2e32a4797a541c
Maggie Bao <maggie.bao202@gmail.com>
2024-05-14 01:44:35 -0400

    completed most of test

---
+++ Distance Measure Test/Select_Acuity.swift
@@ -0,0 +1,26 @@
+//
+//  Select_Acuity.swift
+//  Distance Measure Test
+//
+//  Created by Maggie Bao on 5/14/24.
+//
+
+import UIKit
+
+class Select_Acuity: UIViewController {
+    
+    @IBOutlet weak var B200: UILabel!
+    @IBOutlet weak var B160: UILabel!
+    @IBOutlet weak var B100: UILabel!
+    @IBOutlet weak var B125: UILabel!
+    @IBOutlet weak var B80: UILabel!
+    @IBOutlet weak var B63: UILabel!
+    @IBOutlet weak var B50: UILabel!
+    @IBOutlet weak var B40: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
--- Distance Measure Test/Test.swift
+++ Distance Measure Test/Test.swift
@@ -18,7 +18,7 @@ let ppi: Double = {
 }()
 
 func randomLetters(size: Int) -> String {
-    let validLetters = ["E", "T", "D", "R", "S"]
+    let validLetters = ["S", "K", "H", "N", "O", "C", "D", "V", "R", "Z"]
     
     // Ensure the requested size does not exceed the number of available letters
     guard size <= validLetters.count else {
@@ -64,72 +64,140 @@ func getIndex(numList: [Int], value: Int) -> Int {
     return -1
 }
 
+var transcriptString = "hi"
+
 class Test: UIViewController {
     let averageDistanceCM = 40
+    let acuityList = [200, 160, 125, 100, 80, 63, 50, 40, 32, 25, 20, 16, 12, 10]
+    var currentAcuityIndex = 5
+    var trial = 1
+    var displayLetters: [Int: String] = [:]
+    var userResponses: [Int: String] = [:]
+    var acuityVisits: [Int: Int] = [:]
 
-    @IBOutlet weak var B200: UILabel!
-    @IBOutlet weak var B160: UILabel!
-    @IBOutlet weak var B100: UILabel!
-    @IBOutlet weak var B125: UILabel!
-    @IBOutlet weak var B80: UILabel!
-    @IBOutlet weak var B63: UILabel!
-    @IBOutlet weak var B50: UILabel!
-    @IBOutlet weak var B40: UILabel!
+    
     
     @IBOutlet weak var LetterRow1: UILabel!
     @IBOutlet weak var tempVoiceText: UILabel!
-    var transcriptString = ""
+    
+    var speechRecognizer = SpeechRecognizer()
+    var transcriptString = "hi"
+    var hasTranscript: Bool = false
+    
+    
     var randomLetter: String = "E";
-    var speechRecognizer = SpeechRecognizer();
+//    var speechRecognizer = SpeechRecognizer();
+//    var hasTranscript: Bool = false
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        set_ETDRS(&B200, desired_acuity: 200, letterText: randomLetter)
+//        set_ETDRS(&B160, desired_acuity: 160, letterText: randomLetter)
+//        set_ETDRS(&B100, desired_acuity: 100, letterText: randomLetter)
+//        set_ETDRS(&B125, desired_acuity: 125, letterText: randomLetter)
+//        set_ETDRS(&B80, desired_acuity: 80, letterText: randomLetter)
+//        set_ETDRS(&B63, desired_acuity: 63, letterText: randomLetter)
+//        set_ETDRS(&B50, desired_acuity: 50, letterText: randomLetter)
+//        set_ETDRS(&B40, desired_acuity: 40, letterText: randomLetter)
+//
+//        var transcriptString = "hi"
+//        var startAcuity = 100 // Example value; ensure this is defined based on your application's needs
+//        //set_ETDRS(&LetterRow1, desired_acuity: startAcuity, letterText: randomLetters(size: 5))
+//
+//        var testCond = true
+//        var trial = 1
+////        let acuityList = [200, 160, 125,
+////                          100, 80, 63, 50, 40, 32,
+////                          25, 20, 16, 12, 10]
+//        var index = getIndex(numList: acuityList, value: startAcuity)
+//        var finalVal: Int
+////        var displayLetters = [Int: String]()
+////        var acuityVisits: [Int: Int] = [200: 0, 160: 0, 125: 0,
+////                                           100: 0, 80: 0, 63: 0,
+////                                           50: 0, 40: 0, 32: 0,
+////                                           25: 0, 20: 0, 16: 0,
+////                                           12: 0, 10: 0]
+//
+//        var userLetters = [Int: String]()
+//        while testCond {
+//            //let acuity = acuityList[index]
+//            let acuity = 100
+//            set_ETDRS(&LetterRow1, desired_acuity: acuity, letterText: randomLetters(size: 5))
+//            displayLetters[trial] = LetterRow1.text
+//
+//            print("hello")
+//
+//            while !hasTranscript {
+//              print("ej")
+//            }
+//
+//            userLetters[trial] = transcriptString
+//            print(transcriptString)
+//
+//            acuityVisits[acuity, default: 0] += 1
+//
+//            if acuityVisits[acuity]! > 2 {
+//                finalVal = acuity
+//                testCond = false
+//            }
+//            else {
+//                index += assessInput(inputSeq: displayLetters[trial]!, outputSeq: userLetters[trial]!)
+//                trial += 1
+//            }
+//        }
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        set_ETDRS(&B200, desired_acuity: 200, letterText: randomLetter)
-        set_ETDRS(&B160, desired_acuity: 160, letterText: randomLetter)
-        set_ETDRS(&B100, desired_acuity: 100, letterText: randomLetter)
-        set_ETDRS(&B125, desired_acuity: 125, letterText: randomLetter)
-        set_ETDRS(&B80, desired_acuity: 80, letterText: randomLetter)
-        set_ETDRS(&B63, desired_acuity: 63, letterText: randomLetter)
-        set_ETDRS(&B50, desired_acuity: 50, letterText: randomLetter)
-        set_ETDRS(&B40, desired_acuity: 40, letterText: randomLetter)
-        
-        
-        var startAcuity = 100 // Example value; ensure this is defined based on your application's needs
-        set_ETDRS(&LetterRow1, desired_acuity: startAcuity, letterText: randomLetters(size: 5))
+        initializeLabels()
+    }
     
-        var testCond = true
-        var trial = 1
-        let acuityList = [200, 160, 125,
-                          100, 80, 63, 50, 40, 32,
-                          25, 20, 16, 12, 10]
-        var index = getIndex(numList: acuityList, value: startAcuity)
-        var finalVal: Int
-        var displayLetters = [Int: String]()
-        var acuityVisits: [Int: Int] = [200: 0, 160: 0, 125: 0,
-                                           100: 0, 80: 0, 63: 0,
-                                           50: 0, 40: 0, 32: 0,
-                                           25: 0, 20: 0, 16: 0,
-                                           12: 0, 10: 0]
-
-        var userLetters = [Int: String]()
-        while testCond {
-            let acuity = acuityList[index]
-            //set_ETDRS(&LetterRow1, desired_acuity: acuity, letterText: randomLetters(size: 5))
-            print(type(of: LetterRow1.text))
-            displayLetters[trial] = LetterRow1.text
-            userLetters[trial] = transcriptString
-
-            acuityVisits[acuity, default: 0] += 1
-
-            if acuityVisits[acuity]! > 2 {
-                finalVal = acuity
-                testCond = false
-            }
-            else {
-                index += assessInput(inputSeq: displayLetters[trial]!, outputSeq: userLetters[trial]!)
-                trial += 1
+    func initializeLabels() {
+        for acuity in acuityList {
+            if let label = self.view.viewWithTag(acuity) as? UILabel {
+                var tempLabel: UILabel? = label // Create a modifiable variable
+                set_ETDRS(&tempLabel, desired_acuity: acuity, letterText: "E")
+                acuityVisits[acuity] = 0
             }
         }
+        showNextAcuity()
+    }
+
+    func showNextAcuity() {
+        guard currentAcuityIndex < acuityList.count else { return }
+        let acuity = acuityList[currentAcuityIndex]
+        set_ETDRS(&LetterRow1, desired_acuity: acuity, letterText: randomLetters(size: 5))
+        displayLetters[trial] = LetterRow1.text
+    }
+    
+    func processTranscription() {
+        guard hasTranscript, currentAcuityIndex < acuityList.count else { return }
+        let acuity = acuityList[currentAcuityIndex]
+        userResponses[trial] = transcriptString
+        acuityVisits[acuity, default: 0] += 1
+
+        if assessInput(inputSeq: displayLetters[trial]!, outputSeq: userResponses[trial]!) == 1 {
+            if acuityVisits[acuity]! >= 2 {
+                print("FUCK U")
+                endTest(withAcuity: acuity)
+            } else {
+                currentAcuityIndex += 1
+                trial += 1
+                showNextAcuity()
+            }
+        } else {
+            trial += 1
+            currentAcuityIndex -= 1
+            showNextAcuity()
+        }
+        hasTranscript = false
+    }
+
+    func endTest(withAcuity acuity: Int) {
+        // Process final results and possibly navigate to a results screen
+        print("Test completed with final acuity level: \(acuity)")
     }
     
     @IBAction func startIsPressed(_ sender: Any) {
@@ -137,14 +205,25 @@ class Test: UIViewController {
     }
     
     @IBAction func stopIsPressed(_ sender: Any) {
-        speechRecognizer.stopTranscribing();
-        let transcriptString = speechRecognizer.transcript;
+//        speechRecognizer.stopTranscribing();
+//        transcriptString = speechRecognizer.transcript;
+//        print(transcriptString)
+//        tempVoiceText.text = transcriptString
+//        speechRecognizer.resetTranscript();
+//        hasTranscript = true
+        speechRecognizer.stopTranscribing()
+        transcriptString = speechRecognizer.transcript
         tempVoiceText.text = transcriptString
-        speechRecognizer.resetTranscript();
+        speechRecognizer.resetTranscript()
+        hasTranscript = true
         
     }
     
     @IBAction func nextLineIsPressed(_ sender: Any) {
         
+        processTranscription()
+        print(displayLetters)
+        print(userResponses)
+        print(acuityVisits)
     }
 }
