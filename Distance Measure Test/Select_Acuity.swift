abc8ab1326c6d212100b5b68ea6a1874dfd0a1c9
Maggie Bao <maggie.bao202@gmail.com>
2024-05-28 17:38:12 -0700

    letter fit to size

--- Distance Measure Test/Select_Acuity.swift
+++ Distance Measure Test/Select_Acuity.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class Select_Acuity: UIViewController {
     
-    @IBOutlet weak var B200: UILabel!
-    @IBOutlet weak var B160: UILabel!
-    @IBOutlet weak var B100: UILabel!
-    @IBOutlet weak var B125: UILabel!
-    @IBOutlet weak var B80: UILabel!
-    @IBOutlet weak var B63: UILabel!
-    @IBOutlet weak var B50: UILabel!
-    @IBOutlet weak var B40: UILabel!
+//    @IBOutlet weak var B200: UILabel!
+//    @IBOutlet weak var B160: UILabel!
+//    @IBOutlet weak var B100: UILabel!
+//    @IBOutlet weak var B125: UILabel!
+//    @IBOutlet weak var B80: UILabel!
+//    @IBOutlet weak var B63: UILabel!
+//    @IBOutlet weak var B50: UILabel!
+//    @IBOutlet weak var B40: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
